32347120088945d7670fda1fa162b7c61f164a93
sheha <i.sheeha@gmail.com>
2018-09-26 15:12:21 +0200

    fix #2

--- app/app.js
+++ app/app.js
@@ -17,7 +17,7 @@ import createHistory from 'history/createBrowserHistory';
 import 'sanitize.css/sanitize.css';
 
 // Import root app
-import App from 'containers/App';
+import App from './containers/App';
 
 // Load the favicon
 /* eslint-disable import/no-webpack-loader-syntax */
@@ -53,7 +53,7 @@ if (module.hot) {
   // Hot reloadable React components and translation json files
   // modules.hot.accept does not accept dynamic dependencies,
   // have to be constants at compile-time
-  module.hot.accept(['containers/App'], () => {
+  module.hot.accept(['./containers/App'], () => {
     ReactDOM.unmountComponentAtNode(MOUNT_NODE);
     render();
   });
