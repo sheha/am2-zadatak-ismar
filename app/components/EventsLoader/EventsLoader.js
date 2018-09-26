f5fb8a335c699861bc540924b23d3927f1f57336
sheha <i.sheeha@gmail.com>
2018-09-26 05:14:16 +0200

    flow

--- app/components/EventsLoader/EventsLoader.js
+++ app/components/EventsLoader/EventsLoader.js
@@ -1,28 +1,47 @@
 import React from "react";
-import EventItem from "../EventItem";
 import "./style.scss";
 
-class EventsLoader extends React.Component {
+const IMGPATH = '../../containers/HomePage/statics';
+
+class EventsLoader extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {loading:true};
   }
-
 
   // componentDidMount() {
   //   this.props.loaded();
   // }
 
-
   render() {
+    const loadedEvents = this.props.loaded;
+
+    //let location = `${city} ${region},${country}`;
 
     //console.log(events)
-    console.log(this.state)
-    console.log(this.props)
+    console.log(this.state);
+    console.log(this.props);
     return (
       <div className="events__container">
         <div className="events__content">
-          <EventItem />
+          {loadedEvents.map((event, key) => (
+            <div key={key} className="events__item">
+              <img src={require( `${IMGPATH}`+`${event.imgurl}`)} alt="" />
+              <div className="events__item-info">
+                <div className="events__item-info-time">
+                  <span className="info-time-month"> {event.month}</span>
+                  <span className="info-time-date">{event.date}</span>
+                </div>
+                <div className="events__item-info-place">
+                  <span className="info-place-title">
+                    {event.name}
+                  </span>
+                  <span className="info-place-location">{event.address}</span>
+                  <span className="info-place-city">{`${city} ${region},${country}`}</span>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     );
