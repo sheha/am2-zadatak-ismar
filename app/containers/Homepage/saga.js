a9987c66aaaaf5cf10b76d9736324c941fef9d58
sheha <i.sheeha@gmail.com>
2018-09-27 14:45:55 +0200

    fixes hostname issue

--- app/containers/Homepage/saga.js
+++ app/containers/Homepage/saga.js
@@ -10,9 +10,10 @@ import {
 import { LOAD_EVENTS } from 'containers/App/constants';
 import { eventsLoaded, eventsLoadingError } from './actions';
 
-
+const HOSTNAME = window.location.hostname;
+console.log(HOSTNAME);
 export function* getEvents() {
-  const requestURL = 'http://localhost:3001/events';
+  const requestURL = 'http://'+window.location.hostname + ':3001/events';
 
   try {
     let data = yield call(request, requestURL);
